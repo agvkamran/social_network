46aa8f482a93a84ac24527cd7f83c33a78d3a7a9
Kamran Aghayev <aghayev189@gmail.com>
2021-01-05 14:37:04 +0400

    add users

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import Header from '../src/components/header/header';
 import Navbar from '../src/components/navbar/navbar';
 import Profile from '../src/components/profile/profile';
 import DialogsContainer from './components/dialogs/dialogsContainer';
-import { Route } from 'react-router-dom';
 import MusicContainer from './components/music/musicContainer';
+import UsersContainer from './components/users/usersContainer';
 
-const App = (props) => {
+const App = () => {
   return (
     <div>
       <Header />
@@ -14,6 +15,7 @@ const App = (props) => {
       <Route path='/dialogs' render={() => <DialogsContainer />} />
       <Route path='/profile' render={() => <Profile />} />
       <Route path='/music' render={() => <MusicContainer />} />
+      <Route path='/users' render={() => <UsersContainer />} />
       {/* <Route path='/friends' render={() => <Friends state={props.store.getState().sidebar} dispatch={props.dispatch} />} />
 
       <Route path='/news' render={() => <News state={props.state.siteNews}
@@ -25,15 +27,3 @@ const App = (props) => {
 
 
 export default App;
-// state={props.state.dialogsPage}
-
-
-// profilePage={props.state.profilePage}
-      // <Route path='/friends' render={() => <Friends state={props.state.sidebar}
-      //                                               addNewFriend={props.addNewFriend}
-      //                                               updateNewFriendText={props.updateNewFriendText}
-      //                                               store={props.store} />} />
-                                                    
-      // <Route path='/news' render={() => <News state={props.state.siteNews}
-      //                                         addNewPoint={props.addNewPoint}
-      //                                         updateNewPoint={props.updateNewPoint} />} />
