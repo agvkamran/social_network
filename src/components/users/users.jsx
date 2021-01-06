4f32c99506a2b2fdbadc5e4adcf75087968f82e9
Kamran Aghayev <aghayev189@gmail.com>
2021-01-06 15:27:12 +0400

    pagination

--- src/components/users/users.jsx
+++ src/components/users/users.jsx
@@ -5,13 +5,30 @@ import * as axios from 'axios';
 class Users extends React.Component {
 
     componentDidMount() {
-        alert(123);
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
             this.props.setUsers(response.data.items);
+            this.props.setTotalUsersCount(response.data.totalCount);
         });
     };
-    
+
+            
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items);
+        });
+    }
+
     render() {
+
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize - 1750);
+        let pages = [];
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i);
+        }
+
+
+
         return <div>
             {
                 this.props.users.map(u => <div key={u.id}>
@@ -42,6 +59,9 @@ class Users extends React.Component {
                     </span>
                 </div>)
             }
+                        {pages.map((p) => {
+                return <span onClick={(e) => {this.onPageChanged(p);}} className={this.props.currentPage === p && styles.selected}>{p + ' '}</span>
+            })}
         </div>
     }
 }
